8b06e6fc851c1a02d034dd097fcc2b72fbdcb10b
Asaad Babiker <asaadballah@gmail.com>
2022-09-28 19:03:46 +0800

    add the action logic

--- src/action.js
+++ src/action.js
@@ -1,5 +1,19 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
 async function run(){
-  console.log('Hello from Github Actions');
+  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
+  
+  const octokit = github.getOctokit(GITHUB_TOKEN)
+
+  const { context = {} } = github
+  const { pull_request } = context.payload 
+  
+  await octokit.rest.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: 'Thank you for submitting a pull request! we will try to review it in the upcoming 24 hours',
+  });
 }
 
 run()
